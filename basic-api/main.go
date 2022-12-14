d85a38c84bbf5101222126d1fb4a847748408ba4
codedinsugar <codedinsugar@gmail.com>
2022-12-13 20:46:52 -0500

    Moved comment blocks

--- basic-api/main.go
+++ basic-api/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
@@ -13,10 +14,23 @@ func main() {
 	hh := handlers.NewHello(l)
 	gh := handlers.NewGoodbye(l)
 
+	// https://pkg.go.dev/net/http#NewServeMux
+	// Our own implementation of the DefaultServeMux
+	// Our serve mux will handle all handler paths
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
 	sm.Handle("/goodbye", gh)
 
-	http.ListenAndServe(":9190", sm)
+	// https://pkg.go.dev/net/http#Server
+	// Our own implementation of the default http server
+	// We define the parameters for running an http server
+	s := &http.Server{
+		Addr:         ":9190",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
 
+	s.ListenAndServe() // Start listening for http calls
 }
