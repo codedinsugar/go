b1ad25efd05e1048d8c9ba03b4c85c60b9425606
codedinsugar <codedinsugar@gmail.com>
2022-12-16 07:01:55 -0500

    Added go routine, channel, shutdown

--- basic-api/main.go
+++ basic-api/main.go
@@ -3,14 +3,17 @@ package main
 
 import (
 	"basic-api/handlers" // Provides the handlers package
+	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"time"
 )
 
 func main() {
-	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	l := log.New(os.Stdout, "product-api: ", log.LstdFlags)
 	hh := handlers.NewHello(l)
 	gh := handlers.NewGoodbye(l)
 
@@ -32,5 +35,22 @@ func main() {
 		WriteTimeout: 1 * time.Second,
 	}
 
-	s.ListenAndServe() // Start listening for http calls
+	go func() {
+		fmt.Printf("Starting http server on port%v\n", s.Addr)
+		err := s.ListenAndServe() // Start listening for http calls
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	// Channel for go routine
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }
